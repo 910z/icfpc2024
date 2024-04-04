9d201a093775c214ae1ec6639264ea6b5ef49f81
DevKirill <4599661+devkirill@users.noreply.github.com>
2024-04-05 01:52:43 +0500

    Init server

---
+++ server/kotlin/src/main/kotlin/com/icfpc/IcfpcApplication.kt
@@ -0,0 +1,11 @@
+package com.icfpc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IcfpcApplication
+
+fun main(args: Array<String>) {
+	runApplication<IcfpcApplication>(*args)
+}
