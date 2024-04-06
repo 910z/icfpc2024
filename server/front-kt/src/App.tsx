7260a7914b1d6836a4ed12f27cdb8dbf42830f51
DevKirill <4599661+devkirill@users.noreply.github.com>
2024-04-06 15:48:39 +0500

    Try add custom theme

--- server/front-kt/src/App.tsx
+++ server/front-kt/src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 
-import ButtonsShowcase from './showcases/Buttons';
-import ToastsShowcase from './showcases/Toasts';
+function AlertDismissibleExample() {
+    const [show, setShow] = useState(false);
 
-const App: React.FC = () => {
-  return (
-      <Container className="p-3">
-        <Container className="p-5 mb-4 bg-light rounded-3">
-          <h1 className="header">
-            Welcome To React-Bootstrap TypeScript Example
-          </h1>
+    if (show) {
+        return (
+            <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+                <Alert.Heading>
+                    I am an alert of type <span className="dangerText">danger</span>! But
+                    my color is Teal!
+                </Alert.Heading>
+                <p>
+                    By the way the button you just clicked is an{' '}
+                    <span className="infoText">Info</span> button but is using the color
+                    Tomato. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Accusantium debitis deleniti distinctio impedit officia reprehenderit
+                    suscipit voluptatibus. Earum, nam necessitatibus!
+                </p>
+            </Alert>
+        );
+    }
+    return (
+        <Button variant="info" onClick={() => setShow(true)}>
+            Show Custom Styled Alert
+        </Button>
+    );
+}
+
+const App = () => (
+    <Container className="p-3">
+        <Container className="pb-1 p-5 mb-4 bg-light rounded-3">
+            <h1 className="header">Welcome To React-Bootstrap</h1>
+            <h2 className="header">Using Sass with custom theming</h2>
+            <AlertDismissibleExample />
+            <hr />
+            <p>
+                You can check further in information on the official Bootstrap docs{' '}
+                <a
+                    href="https://getbootstrap.com/docs/4.3/getting-started/theming/#importing"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    here
+                </a>
+                .
+            </p>
         </Container>
-        <h2>Buttons</h2>
-        <ButtonsShowcase />
-        <h2>Toasts</h2>
-        <ToastsShowcase />
-      </Container>
-  );
-};
+    </Container>
+);
 
 export default App;
