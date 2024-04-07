73a8873e7178e3d38745672fa1a8040d62a7f9f6
DevKirill <4599661+devkirill@users.noreply.github.com>
2024-04-07 22:06:30 +0500

    Init table

--- server/front-kt/src/App.tsx
+++ server/front-kt/src/App.tsx
@@ -8,6 +8,8 @@ import ButtonsShowcase from './showcases/Buttons';
 import ToastsShowcase from './showcases/Toasts';
 import {Nav, Navbar, NavDropdown} from "react-bootstrap";
 import ThemeSwitch from "./components/ThemeSwitch";
+import {BrowserRouter, Link, NavLink, Route, Routes} from "react-router-dom";
+import Problems from "./pages/Problems";
 
 function AlertDismissibleExample() {
     const [show, setShow] = useState(false);
@@ -37,56 +39,65 @@ function AlertDismissibleExample() {
 }
 
 const App = () => (
-    <div>
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="#home">ICFPC</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider/>
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-                <ThemeSwitch></ThemeSwitch>
+    <BrowserRouter>
+
+
+        <div>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href={`/`}>ICFPC</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href={`problems`}>Problems</Nav.Link>
+                            <Nav.Link href={`solutions`}>Solutions</Nav.Link>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider/>
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                    <ThemeSwitch></ThemeSwitch>
+                </Container>
+            </Navbar>
+            <Container className="p-3">
+                {/*<Container className="pb-1 p-5 mb-4 bg-light rounded-3">*/}
+                {/*    <h1 className="header">Welcome To React-Bootstrap</h1>*/}
+                {/*    <h2 className="header">Using Sass with custom theming</h2>*/}
+                {/*    <AlertDismissibleExample />*/}
+                {/*    <hr />*/}
+                {/*    <p>*/}
+                {/*        You can check further in information on the official Bootstrap docs{' '}*/}
+                {/*        <a*/}
+                {/*            href="https://getbootstrap.com/docs/4.3/getting-started/theming/#importing"*/}
+                {/*            target="_blank"*/}
+                {/*            rel="noopener noreferrer"*/}
+                {/*        >*/}
+                {/*            here*/}
+                {/*        </a>*/}
+                {/*        .*/}
+                {/*    </p>*/}
+                {/*</Container>*/}
+                {/*<Container className="p-5 mb-4 bg-light rounded-3">*/}
+                {/*    <h1 className="header">*/}
+                {/*        Welcome To React-Bootstrap TypeScript Example*/}
+                {/*    </h1>*/}
+                {/*</Container>*/}
+                <h2>Buttons</h2>
+                <ButtonsShowcase/>
+                <h2>Toasts</h2>
+                <ToastsShowcase/>
+
+                <Routes>
+                    <Route path="problems" element={<Problems/>}/>
+                    <Route path="solutions" element={<ButtonsShowcase/>}/>
+                </Routes>
             </Container>
-        </Navbar>
-        <Container className="p-3">
-            {/*<Container className="pb-1 p-5 mb-4 bg-light rounded-3">*/}
-            {/*    <h1 className="header">Welcome To React-Bootstrap</h1>*/}
-            {/*    <h2 className="header">Using Sass with custom theming</h2>*/}
-            {/*    <AlertDismissibleExample />*/}
-            {/*    <hr />*/}
-            {/*    <p>*/}
-            {/*        You can check further in information on the official Bootstrap docs{' '}*/}
-            {/*        <a*/}
-            {/*            href="https://getbootstrap.com/docs/4.3/getting-started/theming/#importing"*/}
-            {/*            target="_blank"*/}
-            {/*            rel="noopener noreferrer"*/}
-            {/*        >*/}
-            {/*            here*/}
-            {/*        </a>*/}
-            {/*        .*/}
-            {/*    </p>*/}
-            {/*</Container>*/}
-            {/*<Container className="p-5 mb-4 bg-light rounded-3">*/}
-            {/*    <h1 className="header">*/}
-            {/*        Welcome To React-Bootstrap TypeScript Example*/}
-            {/*    </h1>*/}
-            {/*</Container>*/}
-            <h2>Buttons</h2>
-            <ButtonsShowcase/>
-            <h2>Toasts</h2>
-            <ToastsShowcase/>
-        </Container>
-    </div>
+        </div>
+    </BrowserRouter>
 );
 
 export default App;
---
+++ server/front-kt/src/pages/Problems.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Button from 'react-bootstrap/Button';
+import {Table} from "react-bootstrap";
+
+const Problems: React.FC = () => (
+    <Table striped bordered hover>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Preview</th>
+                <th>Instrs</th>
+                <th>Musicns</th>
+                <th>Attends</th>
+                <th>Tastes</th>
+                <th>Pillars</th>
+                <th>Stage Size</th>
+                <th>Score</th>
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </Table>
+);
+
+export default Problems;
