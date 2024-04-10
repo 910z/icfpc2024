66887b3701bd3a8c24ce0cb871524576964abf36
devkirill <4599661+devkirill@users.noreply.github.com>
2024-04-10 11:35:58 +0500

    upd

--- server/kotlin/src/main/kotlin/com/icfpc/controller/MainController.kt
+++ server/kotlin/src/main/kotlin/com/icfpc/controller/MainController.kt
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping
 class MainController {
     @GetMapping("/")
     fun index(): String {
-        return "index"
+        return "redirect:index.html"
     }
 }
