9e3a537a78d6838243fdc2112564919a32586c30
DevKirill <4599661+devkirill@users.noreply.github.com>
2024-04-30 12:25:18 +0500

    Исправлен запрос к БД

--- server/kotlin/src/main/kotlin/com/icfpc/db/repository/SolutionRepository.kt
+++ server/kotlin/src/main/kotlin/com/icfpc/db/repository/SolutionRepository.kt
@@ -8,10 +8,22 @@ import java.math.BigInteger
 interface SolutionRepository : JpaRepository<Solution, Int> {
     @Query("select * from solution where scores -> ?1 is null order by id desc limit ?2", nativeQuery = true)
     fun notCalculated(version: String, limit: Int): List<Solution>
+
+    @Query("select distinct on (solution.problem_id) solution.* " +
+            "from (" +
+            "select problem_id, max(score) as score " +
+            "from solution " +
+            "group by problem_id" +
+            ") maxs " +
+            "inner join solution on solution.problem_id = maxs.problem_id and solution.score = maxs.score", nativeQuery = true)
+    fun bestSolutionList(): List<Solution>
 //
 //    fun findFirstByProblemIdAndScoreIsNotNullOrderByScoreDescIdAsc(): List<Solution>
 }
 
-fun SolutionRepository.bestSolutions() = findAll()
+fun SolutionRepository.bestSolutions() = bestSolutionList()
     .groupBy { it.problemId }
-    .mapValues { list -> list.value.maxBy { it.score ?: BigInteger.ZERO } }
+    .mapValues { it.value.first() }
+//findAll()
+//    .groupBy { it.problemId }
+//    .mapValues { list -> list.value.maxBy { it.score ?: BigInteger.ZERO } }
