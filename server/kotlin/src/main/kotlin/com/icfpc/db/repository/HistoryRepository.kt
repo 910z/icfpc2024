e9544c2ec9e5e9b6e94c0220b7031398f479ce8a
Kirill Deviatkin <4599661+devkirill@users.noreply.github.com>
2024-07-01 10:13:45 +0500

    Update HistoryRepository.kt

--- server/kotlin/src/main/kotlin/com/icfpc/db/repository/HistoryRepository.kt
+++ server/kotlin/src/main/kotlin/com/icfpc/db/repository/HistoryRepository.kt
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
 interface HistoryRepository : JpaRepository<History, String> {
-    @Query("SELECT history FROM History history ORDER BY history.createdAt desc LIMIT 100")
+    @Query("SELECT history FROM History history ORDER BY history.createdAt desc LIMIT 10")
     fun latest(): List<History>
 }
