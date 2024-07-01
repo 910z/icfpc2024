7ee9d40456adabbf7d950b82bed4853d9b687b1b
Kirill Deviatkin <4599661+devkirill@users.noreply.github.com>
2024-07-01 15:41:25 +0500

    Update Util.kt

--- server/kotlin/src/main/kotlin/com/icfpc/lang/Util.kt
+++ server/kotlin/src/main/kotlin/com/icfpc/lang/Util.kt
@@ -5,17 +5,24 @@ object Util {
     val line = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n"
 
     fun parse(command: String): Any? {
-        if (!command.startsWith("S")) return null
-        var num = BigInteger.ZERO
-        for (i in command.drop(1)) {
-            num = num * 94.toBigInteger() + (i.code - 33).toBigInteger()
-        }
-        var s = ""
-        while (num > BigInteger.ZERO) {
-            val a = num % line.length.toBigInteger()
-            s = line[a.toInt()] + s
-            num /= line.length.toBigInteger()
-        }
-        return s
+        return if (command.startsWith("S")) {
+            var num = BigInteger.ZERO
+            for (i in command.drop(1)) {
+                num = num * 94.toBigInteger() + (i.code - 33).toBigInteger()
+            }
+            var s = ""
+            while (num > BigInteger.ZERO) {
+                val a = num % line.length.toBigInteger()
+                s = line[a.toInt()] + s
+                num /= line.length.toBigInteger()
+            }
+            s
+        } else if (command.startsWith("I")) {
+            var num = BigInteger.ZERO
+            for (i in command.drop(1)) {
+                num = num * 94.toBigInteger() + (i.code - 33).toBigInteger()
+            }
+            num.toString()
+        } else null
     }
 }
